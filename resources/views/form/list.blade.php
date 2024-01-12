d953bc28abbb035ae6e2cf79ac36b2735a57de1c
Zahra Abbasi <z3890c34@gmail.com>
2024-01-12 12:46:05 +0330

    add tag html to list-massage-user

---
+++ resources/views/form/list.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="form/tableStyle.css">
+</head>
+
+<body>
+    <div class="div">
+        <table border="1"; class="table">
+
+            <tr>
+                <th>نام و نام خانوادگی</th>
+                <th>ایمیل</th>
+                <th>رمز عبور</th>
+
+            </tr>
+            <tr>
+                <td>bahar kamali</td>
+                <td>email@.com</td>
+                <td>3443</td>
+
+            </tr>
+            <tr>
+                <td>kimia soltany</td>
+                <td>kj@f.com</td>
+                <td>5665</td>
+
+            </tr>
+            <tr>
+                <td>martin kiaii</td>
+                <td>mk@z.com</td>
+                <td>6776</td>
+
+            </tr>
+
+
+        </table>
+        <div>
+</body>
+
+</html>
