87eb0e6d9540ce247130c1df06cf0e808fd73fb3
Zahra Abbasi <z3890c34@gmail.com>
2024-01-12 12:17:53 +0330

    add tag html to edit-user

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="users/user-info.css">
+</head>
+
+<body class="body">
+    <div class="div-1";>
+        <h4 class="h4">ویرایش اطلاعات خود</h4>
+        <form action="" method="POST" class="form">
+            <input type="text" name="" class="text" placeholder="نام و نام خانوادگی">
+            <br>
+            <br>
+            <input type="emai" name="" class="email" placeholder="ایمیل">
+            <br>
+            <br>
+            <input type="password" name="" class="password" placeholder="رمزعبور">
+            <br>
+            <br>
+
+            <input type="text" name="" class="text-2" placeholder="موبایل">
+            <br>
+            <br>
+            <input type="number" name="" class="number" placeholder="مقطع تحصیلی">
+            <br><br>
+
+            <input type="date" name="" class="date">
+
+            <select class="select">
+                <option>استان</option>
+                <option>تهران</option>
+                <option>مشهد</option>
+                <option>اصفهان</option>
+                <option>شیراز</option>
+                <option>تبریز</option>
+            </select>
+
+            <select class="select">
+                <option>شهرستان</option>
+                <option>ورامین</option>
+                <option>رباط کریم</option>
+                <option>شهریار</option>
+                <option>قرچک</option>
+                <option>پیشوا</option>
+            </select>
+            <br>
+            <br>
+            <input type="radio" name="radio" class="radio" value="مرد">
+            زن
+            <input type="radio" name="radio" class="radio" value="زن">
+            مرد
+            <br>
+            <br>
+            <input type="button" name="" class="button" value="ثبت ویرایش">
+
+
+        </form>
+    </div>
+</body>
+
+</html>
