d978946d19df6b0412c061077400a132a2c15520
Zahra Abbasi <z3890c34@gmail.com>
2024-01-12 12:45:01 +0330

    add tag html to send-massage-user

---
+++ resources/views/form/send.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="form/style.css">
+</head>
+
+<body class="body">
+    <div class="div-1";>
+        <h2>ثبت اطلاعات</h2>
+        <form action="" method="POST" class="form">
+            <input type="text" name="" class="text" placeholder="نام و نام خانوادگی">
+            <br>
+            <br>
+            <input type="emai" name="" class="email" placeholder="ایمیل">
+            <br>
+            <br>
+            <input type="password" name="" class="password" placeholder="رمزعبور">
+            <br>
+            <br>
+            <input type="password" name="" class="password-1" placeholder="تکرار رمز عبور">
+            <br>
+            <br>
+            <textarea rows="1" cols="10" placeholder="نظرات خود را ارسال کنید">
+            </textarea>
+            <br>
+
+            <input type="button" name="" class="button" value="ارسال">
+
+
+        </form>
+    </div>
+</body>
+
+</html>
